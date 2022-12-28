f4a25144ed6f8ac6808c5a58065ef8c1b820f703
nastyaGrushko <n.grushko.a@gmail.com>
2022-12-28 19:40:09 +0300

    modified:   src/js/main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,44 @@
-//10
-// const age = +prompt('Ваш возраст: ','');
-// switch (true) {
-//     case age >= 0 && age < 15:
-//         console.log ('Ребёнок')
-//     break
-
-//     case age >= 15 && age < 25:
-//         console.log ('Молодежь')
-//     break
-
-//     case age >= 25 && age < 65:
-//         console.log ('Взрослый')
-//     break
-
-//     case age >= 65 && age <= 120:
-//         console.log ('Пожилой')
-//     break
-    
-//     case age > 120:
-//         console.log ('Сомнительно')
-//     break
-
-//     default:
-//         console.log ('Вводите только цифры')
-//     break    
+//4 
+// const str = prompt ('Введите строку: ', '').split("");
+// console.log (str)
+// function reverse (str) {
+    // let str2 = [];
+    // for (let i = 0; i < str.lenght; i++) {
+    //     str2 = str.reverse()
+    // }
+    // console.log(str2)
 // }
+// const str = prompt ('Введите строку: ', '');
+// reverse(str)
+
+//10
+// function role (age) {
+//     switch (true) {
+//         case age >= 0 && age < 15:
+//             console.log ('Ребёнок')
+//         break
+    
+//         case age >= 15 && age < 25:
+//             console.log ('Молодежь')
+//         break
+    
+//         case age >= 25 && age < 65:
+//             console.log ('Взрослый')
+//         break
+    
+//         case age >= 65 && age <= 120:
+//             console.log ('Пожилой')
+//         break
+        
+//         case age > 120:
+//             console.log ('Сомнительно')
+//         break
+    
+//         default:
+//             console.log ('Вводите только цифры')
+//         break    
+//     }
+// }
+
+// const age = +prompt('Ваш возраст: ','');
+// role(age);
