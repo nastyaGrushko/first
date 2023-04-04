eb9f902848a801f4d807818029187b86bd2149e7
nastyaGrushko <n.grushko.a@gmail.com>
2023-04-04 16:25:25 +0300

    modified:   src/index.html

--- src/js/main.js
+++ src/js/main.js
@@ -36,39 +36,39 @@ $('.why-we_slider').slick({
   nextArrow: $('.fa-chevron-right')
 });
 
-// function getCard ({name, imgPath, price}) {
-//   const card = document.createElement('div');
-//   // card.classList.add('card-container');
-//   const imgContainer = document.createElement('div');
-//   imgContainer.classList.add('product-img-box');
-//   const img = document.createElement('img');
-//   img.src = imgPath;
-//   img.classList.add('product-img');
-//   imgContainer.append(img);
-//   card.appendChild(imgContainer);
-//   const footer = document.createElement('div');
-//   footer.classList.add('card-footer');
-//   const priceEl = document.createElement('div');
-//   priceEl.classList.add('card-price');
-//   priceEl.innerText = price;
-//   const nameEl = document.createElement('div');
-//   nameEl.innerText = name;
-//   nameEl.classList.add('card-name');
-//   footer.append(priceEl, nameEl);
+function getCard ({name, imgPath, price}) {
+  const card = document.createElement('div');
+  // card.classList.add('card-container');
+  const imgContainer = document.createElement('div');
+  imgContainer.classList.add('product-img-box');
+  const img = document.createElement('img');
+  img.src = imgPath;
+  img.classList.add('product-img');
+  imgContainer.append(img);
+  card.appendChild(imgContainer);
+  const footer = document.createElement('div');
+  footer.classList.add('card-footer');
+  const priceEl = document.createElement('div');
+  priceEl.classList.add('card-price');
+  priceEl.innerText = price;
+  const nameEl = document.createElement('div');
+  nameEl.innerText = name;
+  nameEl.classList.add('card-name');
+  footer.append(priceEl, nameEl);
 
-//   card.appendChild(footer);
-//   return card;
-// }
+  card.appendChild(footer);
+  return card;
+}
 
-// const cardOne = getCard({
-//   name: "Блузка женская",
-//   imgPath: "/img/слайдер-акции.jpg",
-//   price: "50.00 руб.",
-// });
+const cardOne = getCard({
+  name: "Блузка женская",
+  imgPath: "/img/слайдер-акции.jpg",
+  price: "50.00 руб.",
+});
 
-// cardOne.className = "product-items_container cardOne-container";
-// // console.log(cardOne.className); //просмотреть текущие значения класса
-// // cardOne.classList.add('meow');
-// // console.log(cardOne.className);
-// // const parent = document.querySelector('.meow');
-// document.querySelector('.meow').appendChild(cardOne)
+cardOne.className = "product-items_container cardOne-container";
+// console.log(cardOne.className); //просмотреть текущие значения класса
+// cardOne.classList.add('meow');
+// console.log(cardOne.className);
+// const parent = document.querySelector('.meow');
+document.querySelector('.meow').appendChild(cardOne)
